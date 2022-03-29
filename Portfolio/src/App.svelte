7155b095514a90e4bbf77e14950ff0128c771db7
raineio <raine.stablum@gmail.com>
2022-03-29 06:19:34 +0200

    thank you @ThaNightHawk for the dropdown in NavBar.svelte

--- Portfolio/src/App.svelte
+++ Portfolio/src/App.svelte
@@ -1,42 +1,27 @@
 <script lang="ts">
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  import NavBar from "./lib/NavBar.svelte"
+  import "bootstrap/dist/css/bootstrap.min.css"
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello Typescript!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  <NavBar />
 </main>
 
 <style>
   :root {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
-
   main {
     text-align: center;
-    padding: 1em;
-    margin: 0 auto;
+    color: gray;
+    padding: 0;
+    margin: 0;
   }
-
   img {
     height: 16rem;
     width: 16rem;
   }
-
   h1 {
     color: #ff3e00;
     text-transform: uppercase;
@@ -46,18 +31,15 @@
     margin: 2rem auto;
     max-width: 14rem;
   }
-
   p {
     max-width: 14rem;
     margin: 1rem auto;
     line-height: 1.35;
   }
-
   @media (min-width: 480px) {
     h1 {
       max-width: none;
     }
-
     p {
       max-width: none;
     }
---
+++ Portfolio/src/lib/NavBar.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+</script>
+
+<main>
+    <div class="d-flex flex-row navbar bg-dark sticky-top">
+        <div class="me-auto ms-3">
+            <a href="#">
+                <img alt="Me :D" height="56" src="raine.png" width="56"/>
+            </a>
+        </div>
+        <div>
+            <a class="btn btn-outline-primary me-3" href="https://github.com/raineio" target="_blank">
+                GitHub
+            </a>
+            <a class="btn btn-outline-primary me-3" href="https://twitter.com/phyxxie_" target="_blank">
+                Twitter
+            </a>
+            <a class="btn btn-outline-primary me-3" href="https://twitch.tv/phyxxie_" target="_blank">
+                Twitch
+            </a>
+        </div>
+        <div class="dropdown me-3">
+            <button class="btn btn-outline-primary dropdown-toggle"
+                    type="button" id="dropdownMenuButton1"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false">
+                Support
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                <li><a class="dropdown-item" href="https://github.com/sponsors/raineio" target="_blank">GitHub Sponsors</a></li>
+                <li><a class="dropdown-item" href="https://ko-fi.com/rainymods" target="_blank">Ko-Fi</a></li>
+            </ul>
+        </div>
+    </div>
+</main>
+
+<style>
+    .container {
+        margin-top: 10px;
+    }
+
+    .dropdown:hover .dropdown-menu {
+        display: block;
+        margin-top: 0;
+    }
+</style>
